946b1226036c4072260ac13100b6c51a8e15ad24
mo1ein <moeinn.com@gmail.com>
2022-08-12 20:43:25 +0430

    Add user to db in start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,17 +1,15 @@
 from plugins.db import database
-from pyrogram import Client, filters
 from configs import START_MESSAGE
+from pyrogram import Client, filters
 
 
 @Client.on_message(filters.command('start') & filters.private)
 async def start(client, message):
-    '''
     my_db = database()
     # database().create_table()
     # if user not exist, add to database
     if my_db.is_user_exist(message.from_user) is False:
         my_db.add_user(message.from_user)
-        print('added user:', message.from_user.id)
+        # print('added user:', message.from_user.id)
     # my_db.show_users()
-    '''
     await message.reply_text(START_MESSAGE)
